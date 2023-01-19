466d9485885fc01280822e8728dd232099032dcf
Thomas Sejner <44927748+ThomasSejner@users.noreply.github.com>
2023-01-19 09:40:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ streamlit.title('My parents Heahty dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('EHard-Boiled Free-Range Egg\'s')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg\'s')
+streamlit.text('🥑🍞 Advokato Toast')
