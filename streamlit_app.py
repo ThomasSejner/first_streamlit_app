c4129d3d820f822275f83bd034dd81ffd33ad51b
Thomas Sejner <44927748+ThomasSejner@users.noreply.github.com>
2023-01-19 09:27:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My parents Heahty dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3  Oak Meal ')
+streamlit.text('Omega 3 \& Oak Meal ')
 streamlit.text('Kale\, Spinat ')
 streamlit.text('EgG\'ss ')
